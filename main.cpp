d28fa7382f58100a40c6c6e6645148b6b987035e
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-11-12 13:15:52 -0800

    1:15 PM

--- main.cpp
+++ main.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
 #include <deque>
+#include <ctime>
+#include <iomanip>
+#include <random>
 #include "Car.h"
 using namespace std;
 
 const int startingsize=2;
+int randomprob();
 
 int main() {
- deque<Car> cars;
- Car startingcar1;
- Car startingcar2;
-cars.push_back(startingcar1);
-cars.push_back(startingcar2);
+    deque<Car> cars;
+    srand(time(0));
+    Car startingcar1;
+    Car startingcar2;
+    cars.push_back(startingcar1);
+    cars.push_back(startingcar2);
+    cout<<endl;
+    cout<<"Inital queue: "<<endl;
+    for(auto& car: cars){
+        car.print();
+    }
+    while(!cars.empty()){
+        if(randomprob()<55){
+            
+        }
+
+    }
 
     return 0; 
+}
+int randomprob(){
+        int rand1= rand() % 100 + 1;
+        return rand1;
 }
